c12aebd16907729e0e2f5b41a9efc0d272e40453
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-08 13:36:34 +0300

    feat: add mock data for developing

--- cats-pinterest/src/entities/cats/api/fetchCatsData.ts
+++ cats-pinterest/src/entities/cats/api/fetchCatsData.ts
@@ -1,15 +1,17 @@
 import { catsListSchema } from "../model/schemas/catsListSchema";
+import { mockedData } from "./mockedData";
 
 const url = import.meta.env.VITE_API_URL;
 const key = import.meta.env.VITE_API_KEY;
 
 export async function fetchCatsData() {
   try {
-    const response = await fetch(
-      `${url}?size=med&mime_types=jpg&format=json&order=ASC&page=0&limit=30`,
-      { method: "GET", headers: { "Content-Type": "application/json", "x-api-key": key } },
-    );
-    const data = await response.json();
+    // const response = await fetch(
+    //   `${url}?size=med&mime_types=jpg&format=json&order=ASC&page=0&limit=30`,
+    //   { method: "GET", headers: { "Content-Type": "application/json", "x-api-key": key } },
+    // );
+    // const data = await response.json();
+    const data = mockedData;
     const typedData = catsListSchema.parse(data);
     console.log(typedData);
     return typedData;
---
+++ cats-pinterest/src/entities/cats/api/mockedData.ts
@@ -0,0 +1,122 @@
+export const mockedData = [
+  {
+    id: "c",
+    url: "https://24.media.tumblr.com/tumblr_krxn0o9cPS1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "d",
+    url: "https://25.media.tumblr.com/tumblr_krww7pEgmK1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "g",
+    url: "https://26.media.tumblr.com/tumblr_krwvziszMn1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "j",
+    url: "https://24.media.tumblr.com/tumblr_krwvxh6KLK1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "n",
+    url: "https://24.media.tumblr.com/tumblr_krvx7tfD0B1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "s",
+    url: "https://28.media.tumblr.com/tumblr_krvvxawUd81qa9hjso1_1280.jpg",
+  },
+  {
+    id: "v",
+    url: "https://29.media.tumblr.com/tumblr_krvvj0ZbSA1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "11",
+    url: "https://25.media.tumblr.com/tumblr_krvv9ssJAe1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "12",
+    url: "https://24.media.tumblr.com/tumblr_krvv7wTHnB1qa9hjso1_1280.jpg",
+  },
+  {
+    id: "1j",
+    url: "https://28.media.tumblr.com/tumblr_lt95ovYgPG1r4xjo2o1_1280.jpg",
+  },
+  {
+    id: "1m",
+    url: "https://27.media.tumblr.com/tumblr_lt9rfcQo7u1r4xjo2o1_1280.jpg",
+  },
+  {
+    id: "3q",
+    url: "https://cdn2.thecatapi.com/images/3q.jpg",
+  },
+  {
+    id: "4d",
+    url: "https://cdn2.thecatapi.com/images/4d.jpg",
+  },
+  {
+    id: "4o",
+    url: "https://cdn2.thecatapi.com/images/4o.jpg",
+  },
+  {
+    id: "4q",
+    url: "https://cdn2.thecatapi.com/images/4q.jpg",
+  },
+  {
+    id: "4v",
+    url: "https://cdn2.thecatapi.com/images/4v.jpg",
+  },
+  {
+    id: "53",
+    url: "https://cdn2.thecatapi.com/images/53.jpg",
+  },
+  {
+    id: "54",
+    url: "https://cdn2.thecatapi.com/images/54.jpg",
+  },
+  {
+    id: "5q",
+    url: "https://cdn2.thecatapi.com/images/5q.jpg",
+  },
+  {
+    id: "62",
+    url: "https://cdn2.thecatapi.com/images/62.jpg",
+  },
+  {
+    id: "66",
+    url: "https://cdn2.thecatapi.com/images/66.jpg",
+  },
+  {
+    id: "68",
+    url: "https://cdn2.thecatapi.com/images/68.jpg",
+  },
+  {
+    id: "6c",
+    url: "https://cdn2.thecatapi.com/images/6c.jpg",
+  },
+  {
+    id: "6e",
+    url: "https://cdn2.thecatapi.com/images/6e.jpg",
+  },
+  {
+    id: "6t",
+    url: "https://cdn2.thecatapi.com/images/6t.jpg",
+  },
+  {
+    id: "78",
+    url: "https://cdn2.thecatapi.com/images/78.jpg",
+  },
+  {
+    id: "9q",
+    url: "https://cdn2.thecatapi.com/images/9q.jpg",
+  },
+  {
+    id: "9v",
+    url: "https://cdn2.thecatapi.com/images/9v.jpg",
+  },
+  {
+    id: "a1",
+    url: "https://cdn2.thecatapi.com/images/a1.jpg",
+  },
+  {
+    id: "a2",
+    url: "https://cdn2.thecatapi.com/images/a2.jpg",
+  },
+];
