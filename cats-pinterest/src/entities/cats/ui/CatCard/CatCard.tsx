daff27c20061c3670843bc3601316b2742ef2236
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-08 15:40:14 +0300

    feat: add favorite button

--- cats-pinterest/src/entities/cats/ui/CatCard/CatCard.tsx
+++ cats-pinterest/src/entities/cats/ui/CatCard/CatCard.tsx
@@ -1,10 +1,12 @@
 import type { Cat } from "../../model/types/Cat";
+import { FavoriteButton } from "../FavoriteButton/FavoriteButton";
 import styles from "./CatCard.module.css";
 
 export function CatCard({ id, url }: Cat) {
   return (
     <div className={styles.card}>
       <img className={styles.image} src={url} alt={`Image of cat named ${id}`} />
+      <FavoriteButton />
     </div>
   );
 }
