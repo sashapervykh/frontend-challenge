4a85a9d0deb81f3f6164ed37c7adb58062a51963
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-09 12:55:55 +0300

    feat: change message on favorite page for consisntent UI

--- cats-pinterest/src/pages/FavoritesPage/FavoritesPage.tsx
+++ cats-pinterest/src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,10 +1,11 @@
 import { useFavoriteCats } from "../../entities/cats/model/hooks/useFavoriteCats";
 import { CatsList } from "../../entities/cats/ui/CatsList/CatsList";
+import { TextMessage } from "../../shared/ui/TextMessage/TextMessage";
 
 export function FavoritesPage() {
   const { favoriteCats } = useFavoriteCats();
   if (!favoriteCats.length) {
-    return "No cats were added to favorites!";
+    return <TextMessage text={"Нет избранных котиков 😿"} />;
   }
   return <CatsList cats={favoriteCats} />;
 }
