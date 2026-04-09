1d0759d0d1df440111a7166c7333d12420b9f597
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-09 11:29:26 +0300

    feat: adjust format of text messages

--- cats-pinterest/src/pages/MainPage/MainPage.tsx
+++ cats-pinterest/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,14 @@
 import { CatsList } from "../../entities/cats/ui/CatsList/CatsList";
 import { useAllCats } from "../../entities/cats/model/hooks/useAllCats";
+import { TextMessage } from "../../shared/ui/TextMessage/TextMessage";
 
 export function MainPage() {
   const { allCats, loading } = useAllCats();
-  if (loading) return "Fetching cats data";
-  if (allCats.length === 0) return "Data has not loaded";
+  if (loading) {
+    return <TextMessage text={"... загружаем котиков для тебя ..."} />;
+  }
+  if (allCats.length === 0) {
+    return <TextMessage text={"Не удалось загрузить котиков. Попробуй еще раз позднее..."} />;
+  }
   return <CatsList cats={allCats} />;
 }
