14481d3f3afbdd2b033182c287e67bcc4fa3bed8
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-09 13:05:00 +0300

    refactor: rename loading message folder and file

--- cats-pinterest/src/pages/MainPage/MainPage.tsx
+++ cats-pinterest/src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import { CatsList } from "../../entities/cats/ui/CatsList/CatsList";
 import { useAllCats } from "../../entities/cats/model/hooks/useAllCats";
 import { TextMessage } from "../../shared/ui/TextMessage/TextMessage";
-import { LoadingMessage } from "../../entities/cats/ui/Loader/Loader";
+import { LoadingMessage } from "../../entities/cats/ui/LoadingMessage/LoadingMessage";
 
 export function MainPage() {
   const { allCats, loading, sentinelRef, error, hasMore } = useAllCats();
