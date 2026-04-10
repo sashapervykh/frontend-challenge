5c84018e3c969a68dba3f85916e8f9771504132c
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-04-10 11:41:36 +0300

    fix: fix import errors

--- cats-pinterest/src/entities/cats/model/contexts/AllCats.context.tsx
+++ cats-pinterest/src/entities/cats/model/contexts/AllCats.context.tsx
@@ -1,3 +1,4 @@
 import { createContext } from "react";
+import type { AllCatsContextType } from "../types/AllCatsContextType";
 
 export const AllCatsContext = createContext<AllCatsContextType | undefined>(undefined);
